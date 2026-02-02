815cebcc38ed19efacf9c9f5bcff504e0345283a
georg haider <georg.haider@artelys.com>
2026-02-02 17:07:34 +0100

    merge opentel with v7.1.0

--- data/crac/crac-io/crac-io-json/src/main/java/com/powsybl/openrao/data/crac/io/json/MinimumViableCrac.java
+++ data/crac/crac-io/crac-io-json/src/main/java/com/powsybl/openrao/data/crac/io/json/MinimumViableCrac.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package com.powsybl.openrao.data.crac.io.json;
 
 import com.fasterxml.jackson.core.JsonFactory;
@@ -10,6 +17,9 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Pattern;
 
+/**
+ * @author Thomas Bouquet {@literal <thomas.bouquet at rte-france.com>}
+ */
 public final class MinimumViableCrac {
 
     private static final Pattern VERSION_PATTERN = Pattern.compile("^[1-9]\\d*\\.\\d+$");
